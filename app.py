9fc70d9ac69e76649f50b71375fd6d7c7988b1f8
ckim48 <ckim48@u.rochester.edu>
2022-09-23 08:52:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,61 +1,74 @@
 from flask import Flask, render_template,redirect, url_for,flash
 import pandas as pd
 import datetime as dt
-
+import sys
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET']) 
 def index():
 	return render_template('index.html')
-
+#/home/ckim48/mysite/
 @app.route('/result/<a>/<b>', methods=['POST', 'GET']) 
 def result(a,b):
-	data = pd.read_csv("/home/ckim48/mysite/static/preprocessed.csv")
+	data = pd.read_csv("static/preprocessed.csv")
 	data['date'] = pd.to_datetime(data['date'])
 
 	if a == "happy":
 		data = data[data["mood"] == "happy"]
 		if b == "sixty":
-			data = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			print("A",file=sys.stderr)
+			# mask = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			data = data[data['date'].between('1900', '1999')]
 		elif b == "zeros":
-			data = data[data['date'].dt.year >1999 & (data['date'].dt.year < 2010)]
+			data = data[data['date'].between('2000', '2009')]
 		elif b == "tenth":
-			data = data[data['date'].dt.year >2009 & (data['date'].dt.year < 2020)]
+			data = data[data['date'].between('2010', '2019')]
 		else:
-			data = data[data['date'].dt.year >2019 & (data['date'].dt.year < 2030)]
+			data = data[data['date'].between('2020', '2030')]
 
+		
 	elif a== "sad":
 		data = data[data["mood"] == "sad"]
 		if b == "sixty":
-			data = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			print("A",file=sys.stderr)
+			# mask = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			data = data[data['date'].between('1900', '1999')]
 		elif b == "zeros":
-			data = data[data['date'].dt.year >1999 & (data['date'].dt.year < 2010)]
+			data = data[data['date'].between('2000', '2009')]
 		elif b == "tenth":
-			data = data[data['date'].dt.year >2009 & (data['date'].dt.year < 2020)]
+			data = data[data['date'].between('2010', '2019')]
 		else:
-			data = data[data['date'].dt.year >2019 & (data['date'].dt.year < 2030)]
+			data = data[data['date'].between('2020', '2030')]
 
 	elif a =="energetic":
-		if b == "sixty":
-			data = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
-		elif b == "zeros":
-			data = data[data['date'].dt.year >1999 & (data['date'].dt.year < 2010)]
-		elif b == "tenth":
-			data = data[data['date'].dt.year >2009 & (data['date'].dt.year < 2020)]
-		else:
-			data = data[data['date'].dt.year >2019 & (data['date'].dt.year < 2030)]
 		data = data[data["mood"] == "energetic"]
-	else:
 		if b == "sixty":
-			data = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			print("A",file=sys.stderr)
+			# mask = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			data = data[data['date'].between('1900', '1999')]
 		elif b == "zeros":
-			data = data[data['date'].dt.year >1999 & (data['date'].dt.year < 2010)]
+			data = data[data['date'].between('2000', '2009')]
 		elif b == "tenth":
-			data = data[data['date'].dt.year >2009 & (data['date'].dt.year < 2020)]
+			data = data[data['date'].between('2010', '2019')]
 		else:
-			data = data[data['date'].dt.year >2019 & (data['date'].dt.year < 2030)]
+			data = data[data['date'].between('2020', '2030')]
+		
+	else:
 		data = data[data["mood"] == "calm"]
-	c = data.sample(n = 8)
+		if b == "sixty":
+			print("A",file=sys.stderr)
+			# mask = data[data['date'].dt.year >1900 & (data['date'].dt.year < 2000)]
+			data = data[data['date'].between('1900', '1999')]
+		elif b == "zeros":
+			data = data[data['date'].between('2000', '2009')]
+		elif b == "tenth":
+			data = data[data['date'].between('2010', '2019')]
+		else:
+			data = data[data['date'].between('2020', '2030')]
+	if len(data) > 8:
+		c = data.sample(n = 8)
+	else:
+		c = data.sample(len(data))
 	dic = c.to_dict()
 
 	artist_list = list(dic["artist"].values())
