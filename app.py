1c43ee7b705a8a4f247d385554feb7de24df1faa
ckim48 <ckim48@u.rochester.edu>
2022-09-25 23:49:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def result2():
 	content_lst.append(data[0]["comments"])
 	content_lst2 = []
 	for i in (0,len(content_lst)):
-		content_lst2.append(content_lst["content"])
+		content_lst2.append(content_lst[i]["content"])
 
 	# global A
 	# A = list(output.keys())
