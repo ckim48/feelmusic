54591b3b14090996aa1c92b0ab20c7283e79060c
ckim48 <ckim48@u.rochester.edu>
2022-09-26 22:10:49 +0900

    Update nlp_runner2.py

--- nlp_runner2.py
+++ nlp_runner2.py
@@ -51,13 +51,13 @@ class nlp_runner:
         #     return list(),list()
         iterobject = iter(hot_python)
         subreddit_objs = []
-        while True:
-            try:
-                subreddit_objs.append(next(iterobject))
-            except NotFound:
-                continue
-            except StopIteration:
-                break
+        # while True:
+        #     try:
+        #         subreddit_objs.append(next(iterobject))
+        #     except NotFound:
+        #         continue
+        #     except StopIteration:
+        #         break
         #     except StopIteration:
         #         break
         #     except prawcore.exceptions.BadRequest:
@@ -66,12 +66,12 @@ class nlp_runner:
         #         pass
         #     except:
         #         pass
-        # for submission in hot_python:
-        #     try:
-        #         print(submission)
-        #         subreddit_objs.append(submission)
-        #     except NotFound:
-        #         break
+        for submission in hot_python:
+            try:
+                subreddit_objs.append(submission)
+            except NotFound:
+                print('Hello world!', file=sys.stderr)
+                continue
         # subreddit_objs = [submission for submission in hot_python]
 
         #subreddit_objs = [submission for submission in hot_python]
